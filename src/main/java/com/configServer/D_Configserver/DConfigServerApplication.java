ff5446adf9ad04003ce97f9a29b3552569a809a6
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-10 15:13:11 +0530

    spring config server innitial commit.

---
+++ src/main/java/com/configServer/D_Configserver/DConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.configServer.D_Configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+public class DConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/configServer/D_Configserver/DConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configServer.D_Configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
